84316062106941eb3b3f80e06352709cb3b93636
john_dev <johnhaimez314@gmail.com>
2022-07-18 11:57:37 +0200

    feat : create component form for verify email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -5,12 +5,5 @@
     <p>Merci de valider votre inscription à l'aide de cette email.</p>
     <p>Vous n'avez pas reçus l'email ?</p>
     
-    {{-- message validation send new email --}}
-    @if (session('status'))
-        <div class="mb-4 font-medium text-sm text-green-600">
-            Un email à été renvoyé
-        </div>
-    @endif
-    {{-- new notifiation for validate account new user --}}
-    <x-form action="{{ route('verification.send') }}" textbtn="Renvoyer l'email de vérification"></x-form>
+    <x-user.form-verify-email/>
 @endsection
